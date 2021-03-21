e4da693dca25a3bb877171de3d1da76d52b39ef3
Lukas Forst <lukas@forst.pw>
2021-03-21 18:35:41 +0100

    add logging when some entities were not processed correctly

--- src/main/kotlin/com/wire/integrations/hold/exports/service/ProcessingService.kt
+++ src/main/kotlin/com/wire/integrations/hold/exports/service/ProcessingService.kt
@@ -7,6 +7,7 @@ import com.wire.integrations.hold.exports.dto.ConversationEvent
 import com.wire.integrations.hold.exports.dto.EnrichedEvent
 import com.wire.integrations.hold.exports.utils.mapCatching
 import mu.KLogging
+import pw.forst.tools.katlib.mapToSet
 
 class ProcessingService(
     private val assetsFileDownloader: AssetsFileDownloader,
@@ -20,10 +21,18 @@ class ProcessingService(
      *
      * Downloads and saves all assets to temporary directory.
      */
-    fun loadAndProcess(): List<EnrichedEvent> =
-        rawEventsRepository.getNotExportedEvents().mapNotNull { event ->
-            eventParser.parse(event)?.let { event to it } // todo maybe log? but we log in parser
-        }.mapCatching(
+    fun loadAndProcess(): List<EnrichedEvent> {
+        val toProcess = rawEventsRepository.getNotExportedEvents()
+        logger.debug { "To be processed: ${toProcess.size}." }
+
+        val processed = toProcess.mapCatching(
+            transform = { event ->
+                eventParser.parse(event)?.let { event to it } // todo maybe log? but we log in parser
+            },
+            errorLog = {
+                "Problem with parsing event ${it.messageId} with type ${it.type}."
+            }
+        ).mapCatching(
             transform = { (rawEvent, conversationEvent) ->
                 when (conversationEvent) {
                     is ConversationEvent.SystemEvent -> {
@@ -55,4 +64,14 @@ class ProcessingService(
                 """.trimIndent()
             }
         )
+
+        logger.debug { "Successfully processed entities ${processed.size}." }
+        if (processed.size != toProcess.size) {
+            val diff = toProcess.mapToSet { it.messageId }
+                .subtract(processed.mapToSet { it.rawEvent.messageId })
+            logger.warn { "Not all data survived processing! Difference was ${diff.size}. The following data:\n${diff.joinToString("\n")}" }
+        }
+
+        return processed
+    }
 }
